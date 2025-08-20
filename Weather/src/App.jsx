db4e7d26b6e3a8312bde29bd713d170c547f5489
Aasheek111 <technicalaashik111@gmail.com>
2025-08-20 13:42:49 +0545

    just starting

--- Weather/src/App.jsx
+++ Weather/src/App.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import { FaSearch } from "react-icons/fa";
 function App() {
-
-  let getWeather= async()=>{
-
-
+  let getWeather = async () => {
     const apiKey = import.meta.env.VITE_API_KEY;
-    
-    let city="pokhara"
-    let URL=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-    
-    try{
-      let response =await fetch(URL);
-      console.log(response.json());
+
+    let city = "pokhara";
+    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    try {
+      let response = await fetch(URL);
+      let data = await response.json();
+    } catch (er) {
+      console.log("error", er);
     }
-    catch(er){
-      console.log("error",er)
-    }
-    
-  }
-  getWeather()
+  };
+  getWeather();
   return (
-    <div className='bg-red-100'>
+    <div className="  bg-[url(./clean.jpg)] bg-no-repeat bg-cover flex flex-col gap-9 justify-center items-center  text-black w-full min-h-screen">
+      <div className="text-4xl lg:text-6xl">WEATHER</div>
+      <div className="bg-blue-500 backdrop-blur-xl p-10 rounded-2xl flex lg:p-15">
+        <input
+          type="text"
+          name=""
+          className="bg-gray-300 text-black outline-none  rounded-2xl p-2 lg:text-3xl lg:p-4 lg:w-3xl"
+          placeholder="Enter the city"
+          id=""
+        />
+        <button className="text-white rounded-2xl text-3xl p-3">
+          <FaSearch />
+        </button>
 
 
+       
+      </div>
+      <div className="data">
+
+City: <span>{data}</span>
+</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
