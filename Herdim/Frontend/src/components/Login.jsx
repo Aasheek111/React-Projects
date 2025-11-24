6dde3045e5b288f87498b8c330ddb7b0b7fb7a47
Aashik Gautam <technicalaashik111@gmail.com>
2025-11-24 16:07:24 +0545

    ui

--- Herdim/Frontend/src/components/Login.jsx
+++ Herdim/Frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useUser } from "./UserContext";
-
+import { SiOnlyfans } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
 function Login() {
   const { setUser, setIslogin } = useUser();
   const [name, setName] = useState("");
@@ -19,47 +20,54 @@ function Login() {
   };
 
   return (
-    <div className="bg-gray-800 h-screen w-full  flex justify-center font-mono text-white  ">
-        <form
-          action=""
-          className="flex flex-col  bg-blue-300 items-center m-20 p-15 rounded-2xl bg-linear-to-r from-cyan-700 to-blue-500"
-          id="form"
-          onSubmit={handelSubmit}
-        >
-          <h1 className="mb-7 text-4xl">
-            Form
-          </h1>
+    <div className="bg-neutral-200 h-screen w-full  flex justify-center font-mono text-black ">
+      <form
+        action=""
+        className="flex flex-col  bg-neutral-100 items-center m-20 p-15 rounded-2xl shadow-neutral-500 shadow-xs"
+        id="form"
+        onSubmit={handelSubmit}
+      >
+        <h1 className="mb-7 text-4xl">Form</h1>
+        Name:
+        <input
+          type="text"
+          name=""
+          autoComplete="off"
+          className="  p-1 border-b-2 outline-none m-2 mb-7"
+          id="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        Room Name:
+        <input
+          type="text"
+          name=""
+          autoComplete="off"
+          className=" border-b-2 p-1 outline-none m-2"
+          id="room"
+          onChange={(e) => {
+            setRoom(e.target.value);
+          }}
+        />
+        <button className="p-2 m-4 border rounded-xl mt-5 w-50 cursor-pointer bg-neutral-900 border-none hover:bg-neutral-800 text-white">
+          Login
+        </button>
+        <div id="error" className="text-red-500">
+          {error}
+        </div>
+        
+        <div className="flex mx-auto shadow-neutral-500 shadow-xs p-2 w-40 mt-7 my-2 rounded-xl  cursor-pointer  hover:bg-neutral-200">
+          Github
+          <FaGithub  size={23} className="mx-2"/>
+        </div>
 
-          
-          Name:
-          <input
-            type="text"
-            name=""
-            autoComplete="off"
-            className="border rounded-2xl p-2 outline-none m-2"
-            id="name"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-          Room Name:
-          <input
-            type="text"
-            name=""
-            autoComplete="off"
-            className="border rounded-2xl p-3 outline-none m-2"
-            id="room"
-            onChange={(e) => {
-              setRoom(e.target.value);
-            }}
-          />
-          <button className="p-2 m-4 border rounded-xl w-30 bg-blue-900 border-none hover:bg-blue-800">Login</button>
-          <div id="error" className="text-red-500">
-            {error}
-          </div>
-        </form>
-      </div>
-
+        <div className="flex mx-auto shadow-xs shadow-neutral-500  mt-1 p-2 w-40 rounded-xl cursor-pointer hover:bg-neutral-200 ">
+          OnlyFans
+          <SiOnlyfans size={23} className="mx-2" />
+        </div>
+      </form>
+    </div>
   );
 }
 
