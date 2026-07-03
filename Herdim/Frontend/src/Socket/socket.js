f906f18768346e42fb733f7a4670e455b29930eb
Aashik Gautam <technicalaashik111@gmail.com>
2026-07-03 11:10:22 +0545

    changes

--- Herdim/Frontend/src/Socket/socket.js
+++
@@ -1,7 +0,0 @@
-import io from "socket.io-client";
-const socket=io("https://herdim-backend.onrender.com/");
-// const socket=io("http://localhost:3000")
-
-//https://herdim-backend.onrender.com/       http://localhost:3000
-
-export default socket;
